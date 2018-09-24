ac5201fc6d66bacf7d635de879cf5f79d6b63f10
MikeyBitey <MikeyBitey@users.noreply.github.com>
2018-09-24 21:33:53 +1000

    Update 2
    small gui buttons added

--- menu.py
+++ menu.py
@@ -11,6 +11,18 @@ filemenu.add_command(label="Close", command=close)
 settingmenu.add_command(label="SpeedTest", command=close)
 menubar.add_cascade(label="File", menu=filemenu)
 menubar.add_cascade(label="Setting", menu=settingmenu)
-
 window.config(menu=menubar)
+
+def callback():
+    print ("click!")
+b=Button(window, text="Basic Speed Test", command=callback)
+b.pack()
+a=Button(window, text="Bandwidth Management", command=callback)
+a.pack()
+c=Button(window, text="Device Management", command=callback)
+c.pack()
+d=Button(window, text="Speed Limiter", command=callback)
+d.pack()
+
+
 window.mainloop()
